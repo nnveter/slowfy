cc84e5c78425180ecb0ff9432fc107f1876f3821
Илюша Илья <nnveter192@gmail.com>
2022-11-29 16:21:04 +0300

    22

--- Slowfy/Views/HomePage.xaml.cs
+++ Slowfy/Views/HomePage.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 using Windows.Media.Core;
 
@@ -13,15 +14,22 @@ namespace XamlBrewer.WinUI3.Navigation.Sample.Views
     public sealed partial class HomePage : Page
     {
         
-        public List<string> track = new List<string>();
+        public List<string> trackName = new List<string>();
+        public List<string> trackTime = new List<string>();
         public HomePage()
         {
             this.InitializeComponent();
             TestView.Items.Add("Mazzelov | Помню");
-            track.Add("file:///C:/Users/Илья/Downloads/mazellovvv_maz_korzh_-_YA_pomnyu_(musmore.com).mp3");
+            trackName.Add("file:///C:/Users/Илья/Downloads/mazellovvv_maz_korzh_-_YA_pomnyu_(musmore.com).mp3");
             TestView.Items.Add("Шарлот | Щека на щеку");
-            track.Add("file:///C:/Users/Илья/Downloads/v0_10056371543_1_1.mp3");
-
+            trackName.Add("file:///C:/Users/Илья/Downloads/v0_10056371543_1_1.mp3");
+            Player.TransportControls.IsZoomButtonVisible = false;
+            Player.TransportControls.IsZoomEnabled = false;
+            Player.TransportControls.IsPlaybackRateButtonVisible = false;
+            Player.TransportControls.IsNextTrackButtonVisible = true;
+            Player.TransportControls.IsPreviousTrackButtonVisible = true;
+            Player.TransportControls.IsPlaybackRateEnabled = true;
+            Player.TransportControls.IsCompact = true;
         }
 
         
@@ -54,12 +62,12 @@ namespace XamlBrewer.WinUI3.Navigation.Sample.Views
         }
 
         // Handles removal of items in the List.
-        private async void TestView_SelectionChanged(object sender, SelectionChangedEventArgs e) // Event handler
+        private void TestView_SelectionChanged(object sender, SelectionChangedEventArgs e) // Event handler
         {
             // Looking at if the list is anything more than 0 items, they can be removed
             if (TestView.SelectedIndex > -1)
             {
-                Player.Source = MediaSource.CreateFromUri(new Uri(track[TestView.SelectedIndex]));
+                Player.Source = MediaSource.CreateFromUri(new Uri(trackName[TestView.SelectedIndex]));
             }
         }
 
